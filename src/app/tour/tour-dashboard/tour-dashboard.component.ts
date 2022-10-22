d6fa2d32874b19c453cb76f571ed5b1714e6ca8e
sarayut jansoda <sarayut.jansoda@gmail.com>
2022-10-22 23:05:27 +0700

    feat(TourDashBoard): tour Dasboard

--- src/app/tour/tour-dashboard/tour-dashboard.component.ts
+++ src/app/tour/tour-dashboard/tour-dashboard.component.ts
@@ -1,15 +1,119 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { ApexAxisChartSeries, ApexChart, ApexDataLabels, ApexFill, ApexLegend, ApexNonAxisChartSeries, ApexPlotOptions, ApexStroke, ApexTooltip, ApexXAxis, ApexYAxis, ChartComponent } from 'ng-apexcharts';
 
+export type ChartOptions = {
+  // plotOptions: ApexPlotOptions;
+  series: ApexAxisChartSeries;
+  labels: string[];
+  chart: ApexChart;
+  legend: ApexLegend;
+  // lastUpdateFeed: Date;
+  // lastUpdateUnMatch: Date;
+  // colors: string[];
+  plotOptions: ApexPlotOptions;
+  dataLabels: ApexDataLabels;
+  stroke: ApexStroke;
+  xaxis: ApexXAxis;
+  yaxis: ApexYAxis;
+  fill: ApexFill;
+  tooltip: ApexTooltip;
+  // title: ApexTitleSubtitle;
+};
 @Component({
   selector: 'app-tour-dashboard',
   templateUrl: './tour-dashboard.component.html',
   styleUrls: ['./tour-dashboard.component.scss']
 })
 export class TourDashboardComponent implements OnInit {
+  displayedColumns: string[] = [
+    'bookindId',
+    'refName',
+    'total',
+    'status',
+    'location'
+  ];
+
+  @ViewChild("chart") chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
+
+
+  dataSource = new MatTableDataSource();
 
   constructor() { }
 
   ngOnInit(): void {
+    this.chartOptions = {
+      series: [
+        {
+          name: 'Net Profit',
+          data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+        }, {
+          name: 'Revenue',
+          data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+        }, {
+          name: 'Free Cash Flow',
+          data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
+        }
+      ],
+      chart: {
+        type: 'bar',
+        height: 350,
+        stacked: true,
+        toolbar: {
+          show: true
+        },
+        events: {
+          dataPointSelection: ((event, chartContext, config)=>{
+            console.log(config.w.config.xaxis.categories[config.dataPointIndex]);
+            console.log(config.w.config.series[config.seriesIndex].data[config.dataPointIndex]);
+          })
+        }
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          borderRadius: 10,
+          dataLabels: {
+            position: 'center'
+          }
+        },
+      },
+      dataLabels: {
+        enabled: true,
+        style: {
+          fontSize: '13px',
+          fontWeight: 900
+        }
+      },
+      legend: {
+        position: 'right',
+        offsetY: 40
+      },
+      stroke: {
+        show: true,
+        width: 2,
+        colors: ['transparent']
+      },
+      xaxis: {
+        categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+      },
+      yaxis: {
+        title: {
+          text: '$ (thousands)'
+        }
+      },
+      fill: {
+        opacity: 1
+      },
+      tooltip: {
+        y: {
+          formatter: function (val) {
+            return "$ " + val + " thousands"
+          }
+        }
+      }
+    };
   }
 
 }
