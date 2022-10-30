063d5f6400dd89c9e2703622f89a45cb72a77622
sarayut jansoda <sarayut.jansoda@gmail.com>
2022-10-30 21:59:11 +0700

    Feat(Tour): Change some not

--- src/app/tour/tour-dashboard/tour-dashboard.component.ts
+++ src/app/tour/tour-dashboard/tour-dashboard.component.ts
@@ -54,10 +54,28 @@ export class TourDashboardComponent implements OnInit {
         {
           name: 'Net Profit',
           data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
-        }, {
+        }, 
+        {
           name: 'Revenue',
           data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
-        }, {
+        }, 
+        {
+          name: 'Revenue',
+          data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+        }, 
+        {
+          name: 'Revenue',
+          data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+        }, 
+        {
+          name: 'Revenue',
+          data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+        }, 
+        {
+          name: 'Revenue',
+          data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+        }, 
+        {
           name: 'Free Cash Flow',
           data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
         }
